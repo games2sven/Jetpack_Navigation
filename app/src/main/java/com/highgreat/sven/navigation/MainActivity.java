83d4fbd26b27cc490a4a340a44d349e397e86878
zhouli <zhouli@hg-fly.com>
2020-03-31 14:15:59 +0800

    jetpack之navigation学习

---
+++ app/src/main/java/com/highgreat/sven/navigation/MainActivity.java
@@ -0,0 +1,23 @@
+package com.highgreat.sven.navigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavHost;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.my_nav_host_fragment);
+        return NavHostFragment.findNavController(fragment).navigateUp();
+    }
+}
